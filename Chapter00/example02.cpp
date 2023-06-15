ba64bd624756638f12cb7477fed441140923cb72
Dplo1514 <dladlsgur3334@gmail.com>
2023-06-15 23:40:34 +0900

    02_C언어로 배우는 자료구조 문자열

--- Chapter00/example02.cpp
+++ Chapter00/example02.cpp
@@ -4,3 +4,20 @@
 
 #include <cstdio>
 
+
+/*
+ * 문자열
+    * 문자열은 char Type 배열에 하나씩 저장됨
+*/
+int main() {
+    char str[6];
+    str[0] = 'h';
+    str[1] = 'e';
+    str[2] = 'l';
+    str[3] = 'l';
+    str[4] = 'o';
+    str[5] = '\0'; // null character('\0')은 문자열의 끝을 표시하는 역할을 한다, 즉 배열의 크기가 문자열의 길이보다 적어도 1만큼 길어야한다.
+
+    char str1[] = "hello"; // c 언어는 문자열을 생성하는 편리한 방법을 제공한다.
+    char *str1 = "hello";  // 하지만 위와 같이 정의된 문자열은 수정이 불가능하다는 점에서 위의 두 방법과 다르다 이것을 String literal 이라고 부른다.
+}
